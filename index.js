96f38f1ab0957347a06169d31306c1ccd9dad50c
James <jamesklauck@Jamess-MacBook-Air.local>
2021-12-14 09:05:07 -0500

    commit tree a962ada4b900c15fcb654651a0b272ffd45accae

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    
+return customerName.toUpperCase()
+
+}
+
+function setBestCustomer(){
+overwriteBestCustomer() {
+
+}
+}
+
+upperCaseCustomerName()
+
+
+
